5fcdc7c88c88d7dee623f95e266c07fddb415023
Jessy Tranchant <jtranchant@sio.local>
2020-10-07 17:39:07 +0200

    Ajout de fonction

--- software.php
+++ software.php
@@ -2,7 +2,7 @@
 require_once("include/datas.inc.php");
 const DATA_LOCATION='datas/';
 echo "----------------------Gestion des Licences logicielles--------------------------\n";
-
+echo "test";
 
 
 $employees=loadAndDump(DATA_LOCATION."employees.json");
@@ -13,6 +13,11 @@ $licenses=loadAndDump(DATA_LOCATION."licenses.json");
 
 arrayDump(getEmployeesByService($employees,'compta'),'Liste des employés de la compta');
 
+//Test Where
+arrayDump(where($employees,'service', "dsi"),'Liste des employés DSI');
+//Test select
+arrayDump(select($licenses,["name","count"]), 'Nombre de licences');
 
 
-
+//Test SelectWhere
+arrayDump(selectWhere($softs, ['name'], ['count',0]), 'Softs avec 0 licences');
